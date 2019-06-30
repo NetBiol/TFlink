cb46a07607781ad20bfeb77fbc3394683c5b4438
András Hidas <hidasandris@caesar.elte.hu>
2019-06-30 09:47:30 +0200

    Update publish_to_ghpages.sh

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -13,7 +13,7 @@ git worktree prune
 rm -rf .git/worktrees/dist/
 
 echo "Checking out gh-pages branch into dist"
-git worktree add -B gh-pages dist upstream/gh-pages
+git worktree add -B gh-pages dist origin/gh-pages
 
 echo "Removing existing files"
 rm -rf dist/*
@@ -24,5 +24,5 @@ npm run-script build
 echo "Updating gh-pages branch"
 cd dist && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
-#git push --all
+echo "Pushing to github"
+git push --all
