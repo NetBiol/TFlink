5976276e173d6a044b8f646cf53dc2cb25214842
András Hidas <hidasandris@gmail.com>
2021-01-19 22:06:10 +0100

    capitalized protein names of C. elegans

--- src/network.js
+++ src/network.js
@@ -1,25 +1,30 @@
 import cytoscape from "cytoscape";
 import coseBilkent from "cytoscape-cose-bilkent";
 
-$(document).ready(function() {
+$(document).ready(function () {
   cytoscape.use(coseBilkent);
   const baseId = "node0";
   const targetId = "target_data";
   const tfId = "tf_data";
+  const organism = $("#organism").text();
 
   function createNodes(cy, dataId, target = false) {
-    const base_protein = $("#protein_name").text().split(';')[0];
+    let base_protein = $("#protein_name").text().split(";")[0];
+    base_protein =
+      organism === "Caenorhabditis elegans"
+        ? base_protein.toUpperCase()
+        : base_protein;
     const nodes = $("#" + dataId)
       .val()
       .split("\n")
-      .map(row => row.split(",")[0].trim());
+      .map((row) => row.split(",")[0].trim());
     const uniqueNodes = [...new Set(nodes)];
     cy.add({
       group: "nodes",
       data: {
         id: baseId,
-        name: base_protein
-      }
+        name: base_protein,
+      },
     });
     let nodeId = 1;
     for (let node of uniqueNodes) {
@@ -28,17 +33,19 @@ $(document).ready(function() {
           group: "edges",
           data: {
             source: baseId,
-            target: baseId
-          }
+            target: baseId,
+          },
         });
       } else {
-        node = node.split(';')[0];
+        node = node.split(";")[0];
+        node =
+          organism === "Caenorhabditis elegans" ? node.toUpperCase() : node;
         cy.add({
           group: "nodes",
           data: {
             id: `node${nodeId}`,
-            name: node
-          }
+            name: node,
+          },
         });
 
         target
@@ -46,20 +53,20 @@ $(document).ready(function() {
               group: "edges",
               data: {
                 source: baseId,
-                target: `node${nodeId++}`
-              }
+                target: `node${nodeId++}`,
+              },
             })
           : cy.add({
               group: "edges",
               data: {
                 source: `node${nodeId++}`,
-                target: baseId
-              }
+                target: baseId,
+              },
             });
       }
     }
     cy.layout({
-      name: "cose-bilkent"
+      name: "cose-bilkent",
     }).run();
   }
 
@@ -68,28 +75,28 @@ $(document).ready(function() {
       selector: "node",
       style: {
         height: 40,
-        width: function(ele) {
+        width: function (ele) {
           return ele.data("id").length * 10 + 20;
         },
         label: "data(name)",
         "text-halign": "center",
-        "text-valign": "center"
-      }
+        "text-valign": "center",
+      },
     },
 
     {
       selector: "edge",
       style: {
         width: 2,
-        "curve-style": "haystack",
+        "curve-style": "bezier",
         "line-color": "#ccc",
         "target-arrow-color": "#ccc",
-        "target-arrow-shape": "triangle"
-      }
-    }
+        "target-arrow-shape": "triangle",
+      },
+    },
   ];
   const cyLayout = {
-    name: "cose-bilkent"
+    name: "cose-bilkent",
   };
   const cyWheelSensitivity = 0.5;
 
@@ -100,7 +107,7 @@ $(document).ready(function() {
           container: $("#target-network-div"),
           style: cyStyle,
           layout: cyLayout,
-          wheelSensitivity: cyWheelSensitivity
+          wheelSensitivity: cyWheelSensitivity,
         })
       : null;
   let cyTf =
@@ -109,22 +116,19 @@ $(document).ready(function() {
           container: $("#tf-network-div"),
           style: cyStyle,
           layout: cyLayout,
-          wheelSensitivity: cyWheelSensitivity
+          wheelSensitivity: cyWheelSensitivity,
         })
       : null;
 
   if (cyTarget) {
-    cyTarget
-      .style()
-      .selector("node")
-      .style({
-        "background-color": "#70adb5"
-      });
+    cyTarget.style().selector("node").style({
+      "background-color": "#70adb5",
+    });
     cyTarget
       .style()
       .selector(`[id = '${baseId}']`)
       .style({
-        "background-color": "#f0506e"
+        "background-color": "#f0506e",
       })
       .update();
 
@@ -132,7 +136,7 @@ $(document).ready(function() {
     const targetControlDiv = webix.ui({
       container: "targetControlDiv",
       css: {
-        "background-color": "transparent !important"
+        "background-color": "transparent !important",
       },
       view: "form",
       borderless: true,
@@ -146,12 +150,12 @@ $(document).ready(function() {
           width: 35,
           value: "Download image",
           tooltip: true,
-          click: function() {
+          click: function () {
             webix.html.download(
               cyTarget.png(),
               "TFLink_" + $("#uniprot-ac").text() + ".png"
             );
-          }
+          },
         },
         {
           view: "button",
@@ -160,9 +164,9 @@ $(document).ready(function() {
           width: 35,
           value: "Fit network",
           tooltip: true,
-          click: function() {
+          click: function () {
             cyTarget.fit();
-          }
+          },
         },
         {
           view: "button",
@@ -171,9 +175,9 @@ $(document).ready(function() {
           width: 35,
           value: "Zoom +",
           tooltip: true,
-          click: function() {
+          click: function () {
             cyTarget.zoom(cyTarget.zoom() + 0.5);
-          }
+          },
         },
         {
           view: "button",
@@ -182,33 +186,30 @@ $(document).ready(function() {
           width: 35,
           value: "Zoom -",
           tooltip: true,
-          click: function() {
+          click: function () {
             cyTarget.zoom(cyTarget.zoom() - 0.5);
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
   }
 
   if (cyTf) {
-    cyTf
-      .style()
-      .selector("node")
-      .style({
-        "background-color": "#f0506e"
-      });
+    cyTf.style().selector("node").style({
+      "background-color": "#f0506e",
+    });
     cyTf
       .style()
       .selector(`[id = '${baseId}']`)
       .style({
-        "background-color": "#70adb5"
+        "background-color": "#70adb5",
       })
       .update();
     createNodes(cyTf, tfId);
     const tfControlDiv = webix.ui({
       container: "tfControlDiv",
       css: {
-        "background-color": "transparent !important"
+        "background-color": "transparent !important",
       },
       view: "form",
       borderless: true,
@@ -222,12 +223,12 @@ $(document).ready(function() {
           width: 35,
           value: "Download image",
           tooltip: true,
-          click: function() {
+          click: function () {
             webix.html.download(
               cyTf.png(),
               "TFLink_" + $("#uniprot-ac").text() + ".png"
             );
-          }
+          },
         },
         {
           view: "button",
@@ -236,9 +237,9 @@ $(document).ready(function() {
           width: 35,
           value: "Fit network",
           tooltip: true,
-          click: function() {
+          click: function () {
             cyTf.fit();
-          }
+          },
         },
         {
           view: "button",
@@ -247,9 +248,9 @@ $(document).ready(function() {
           width: 35,
           value: "Zoom +",
           tooltip: true,
-          click: function() {
+          click: function () {
             cyTf.zoom(cyTf.zoom() + 0.5);
-          }
+          },
         },
         {
           view: "button",
@@ -258,11 +259,11 @@ $(document).ready(function() {
           width: 35,
           value: "Zoom -",
           tooltip: true,
-          click: function() {
+          click: function () {
             cyTf.zoom(cyTf.zoom() - 0.5);
-          }
-        }
-      ]
+          },
+        },
+      ],
     });
   }
 });
