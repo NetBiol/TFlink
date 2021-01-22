bf3edf41a554548fd4ce15ea28a87431d63177b8
András Hidas <hidasandris@gmail.com>
2021-01-22 14:40:23 +0100

    formatting

--- src/browse.js
+++ src/browse.js
@@ -1,31 +1,29 @@
-var data_url_prefix = '';
-
 webix.ready(function () {
   var dtable = new webix.ui({
-    container: 'browsetable',
-    id: 'dtable',
-    view: 'datatable',
-    css: '',
+    container: "browsetable",
+    id: "dtable",
+    view: "datatable",
+    css: "",
     columns: [
       {
-        id: 'Organism',
-        map: '#data0#',
+        id: "Organism",
+        map: "#data0#",
         header: [
-          'Organism',
+          "Organism",
           {
-            content: 'selectFilter',
+            content: "selectFilter",
           },
         ],
         width: 200,
         tooltip: false,
       },
       {
-        id: 'protein_name',
-        map: '#data1#',
+        id: "protein_name",
+        map: "#data1#",
         header: [
-          'Name',
+          "Name",
           {
-            content: 'textFilter',
+            content: "textFilter",
           },
         ],
         adjust: true,
@@ -34,66 +32,63 @@ webix.ready(function () {
         fillspace: true,
       },
       {
-        id: 'uniprot_id',
-        map: '#data2#',
+        id: "uniprot_id",
+        map: "#data2#",
         header: [
-          'Uniprot ID',
+          "Uniprot ID",
           {
-            content: 'textFilter',
+            content: "textFilter",
           },
         ],
         width: 110,
         tooltip: false,
       },
       {
-        id: 'ncbi_gene_id',
-        map: '#data3#',
+        id: "ncbi_gene_id",
+        map: "#data3#",
         header: [
-          'NCBI Gene ID',
+          "NCBI Gene ID",
           {
-            content: 'textFilter',
+            content: "textFilter",
           },
         ],
         width: 140,
       },
       {
-        id: 'function',
-        map: '#data4#',
+        id: "function",
+        map: "#data4#",
         header: [
-          'Function',
+          "Function",
           {
-            content: 'selectFilter',
+            content: "selectFilter",
           },
         ],
         width: 260,
-        tooltip: false
+        tooltip: false,
       },
     ],
-    url: data_url_prefix + 'data/browse_tables/all_species_LT_browse_update.csv',
+    url: "data/browse_tables/all_species_LT_browse_update.csv",
     resizeColumn: true,
-    datatype: 'csv',
+    datatype: "csv",
     fixedRowHeight: false,
     autoheight: true,
     scroll: false,
     tooltip: true,
     pager: {
-      css: '',
+      css: "",
       template:
-        '{common.first()} {common.prev()} {common.pages()} {common.next()} {common.last()}',
-      container: 'paging_here',
+        "{common.first()} {common.prev()} {common.pages()} {common.next()} {common.last()}",
+      container: "paging_here",
       size: 25,
       group: 5,
     },
-    hover: 'browse_row_hover',
+    hover: "browse_row_hover",
     on: {
       onItemClick: function (id, e, trg) {
         window.open(
-          data_url_prefix +
-            'protein/' +
-            dtable.getItem(id.row).uniprot_id.toLowerCase() +
-            '/'
+          "protein/" + dtable.getItem(id.row).uniprot_id.toLowerCase() + "/"
         );
       },
-    }
+    },
   });
 });
