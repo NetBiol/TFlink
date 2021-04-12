b4cbc6d99cc7322b0d2da74d8a41bc1a3ccc6854
András Hidas <hidasandris@gmail.com>
2021-04-12 17:23:15 +0200

    download page updated with all scale, download file links migrated to cdn

--- src/download.js
+++ src/download.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+  const cdnUrl = 'https://cdn.netbiol.org/tflink/download_files/TFLink';
   let speciesList = [];
   $.ajax({
     url: '/data/spec_list.json',
@@ -12,7 +13,9 @@ $(document).ready(function () {
   const formats = [
     'interaction table',
     'interaction MITAB',
-    'interaction GMT',
+    'interaction GMT uniprotID',
+    'interaction GMT NCBI GeneID',
+    'interaction GMT protein name',
     'binding site table',
     'binding site sequences',
   ];
@@ -21,6 +24,8 @@ $(document).ready(function () {
     'ss_interaction_table',
     'ss_interaction_mitab',
     'ss_interaction_gmt',
+    'ss_interaction_gmt_ncbi',
+    'ss_interaction_gmt_name',
     'ss_binding_site_table',
     'ss_binding_site_sequence',
   ];
@@ -28,8 +33,19 @@ $(document).ready(function () {
     'ls_interaction_table',
     'ls_interaction_mitab',
     'ls_interaction_gmt',
+    'ls_interaction_gmt_ncbi',
+    'ls_interaction_gmt_name',
     'ls_binding_site_table',
     'ls_binding_site_sequence',
+  ];
+  const allDownloadFormats = [
+    'all_interaction_table',
+    'all_interaction_mitab',
+    'all_interaction_gmt',
+    'all_interaction_gmt_ncbi',
+    'all_interaction_gmt_name',
+    'all_binding_site_table',
+    'all_binding_site_sequence',
   ];
 
   var species_select = [
@@ -64,6 +80,7 @@ $(document).ready(function () {
     function (species) {
       let htmlSS = '<h3>Small-scale</h3>';
       let htmlLS = '<h3>Large-scale</h3>';
+      let htmlAll = '<h3>All</h3>';
       const index = speciesList.findIndex(function (item, i) {
         return item.id === species;
       });
@@ -74,31 +91,38 @@ $(document).ready(function () {
           htmlSS += '<p>&nbsp; <br/> &nbsp;</p>';
           return;
         }
-
-        let url = downloadFiles[species][key].startsWith('http')
-          ? downloadFiles[species][key]
-          : (url = `/data/download/TFLink_${species}_${downloadFiles[species][key]}`);
+        let url = `${cdnUrl}_${species}_${downloadFiles[species][key]}`;
 
         htmlSS += `<p><a href="${url}"><i>${speciesList[index].value}</i> small-scale ${formats[i]}</a></p>`;
       });
+
       lsDownloadFormats.forEach((key, i) => {
         if (i == 3) htmlLS += '<p>&nbsp;</p>';
         if (!downloadFiles[species].hasOwnProperty(key)) {
           htmlLS += '<p>&nbsp; <br/> &nbsp;</p>';
           return;
         }
-
-        let url = downloadFiles[species][key].startsWith('http')
-          ? downloadFiles[species][key]
-          : (url = `/data/download/TFLink_${species}_${downloadFiles[species][key]}`);
+        let url = `${cdnUrl}_${species}_${downloadFiles[species][key]}`;
 
         htmlLS += `<p><a href="${url}"><i>${speciesList[index].value}</i> large-scale ${formats[i]}</a></p>`;
+      });
+
+      allDownloadFormats.forEach((key, i) => {
+        if (i == 3) htmlAll += '<p>&nbsp;</p>';
+        if (!downloadFiles[species].hasOwnProperty(key)) {
+          htmlAll += '<p>&nbsp; <br/> &nbsp;</p>';
+          return;
+        }
+        let url = `${cdnUrl}_${species}_${downloadFiles[species][key]}`;
+
+        htmlAll += `<p><a href="${url}"><i>${speciesList[index].value}</i> small and large-scale ${formats[i]}</a></p>`;
       });
 
       let siluette = `<img src="/images/siluettes/${species}.svg" />`;
 
       $('#download_div_ss').html(htmlSS);
       $('#download_div_ls').html(htmlLS);
+      $('#download_div_all').html(htmlAll);
       $('#species_siluette').html(siluette);
     }
   );
